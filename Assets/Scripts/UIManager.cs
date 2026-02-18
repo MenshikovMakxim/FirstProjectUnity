956e406d610592333c50ccdf8eb54a0d9613cb8b
MenshikovMakxim <menshikov5254@gmail.com>
2026-02-18 15:40:19 +0200

    Додано сторінку Credits

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,16 +4,24 @@ public class UIManager : MonoBehaviour
 {
     public GameObject menuPanel;
     public GameObject gamePanel;
+    public GameObject creditsPanel;
 
     public void OpenGame(){
         menuPanel.SetActive(false);
+        creditsPanel.SetActive(false);
         gamePanel.SetActive(true);
     }
 
     public void OpenMenu(){
         menuPanel.SetActive(true);
+        creditsPanel.SetActive(false);
         gamePanel.SetActive(false);
     }
 
+    public void OpenCredits(){
+        menuPanel.SetActive(false);
+        gamePanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
     
 }
